f5b6dea002ffb1dd0a943dcb193c410be8cd62d7
KasperSvendsen <kasperps95@gmail.com>
2023-12-04 10:49:21 +0100

    Added currency column

--- generate_template.py
+++ generate_template.py
@@ -3,7 +3,6 @@ from openpyxl import Workbook, load_workbook
 from openpyxl.worksheet.datavalidation import DataValidation
 import openpyxl
 
-
 def get_document_name():
     return input("Enter the name of the document: ")
 
@@ -25,16 +24,18 @@ def get_person_names(size):
         names.append(name)
     return names
 
-def create_expense_template(doc_name, people_names):
-    data = {'Paying person': [], 'Description': [], 'Amount': [], 'Shared with': []}
-    
-    num_blank_lines = 10
+def get_default_currency():
+    return input("Enter the default currency code (e.g., EUR): ")
 
+def create_expense_template(doc_name, people_names, default_currency):
+    data = {'Paying person': [], 'Description': [], 'Amount': [], 'Currency': [], 'Shared with': []}
+    
     # Add 10 blank lines
-    for _ in range(num_blank_lines):
+    for _ in range(10):
         data['Paying person'].append('')
         data['Description'].append('')
         data['Amount'].append('')
+        data['Currency'].append(default_currency)
         data['Shared with'].append(', '.join(people_names))
     
     df = pd.DataFrame(data)
@@ -53,7 +54,7 @@ def create_expense_template(doc_name, people_names):
     sheet = book.active
     dv = DataValidation(type="list", formula1=f'"{", ".join(people_names)}"', allow_blank=True)
     sheet.add_data_validation(dv)
-    dv.add(f'A2:A{num_blank_lines + 1}') # Add 1 because of the header
+    dv.add(f'A2:A{len(people_names)+1}')  # Assuming A is the column for "Paying person"
     
     # Set the width of the columns
     for i, column_cells in enumerate(sheet.columns):
@@ -68,7 +69,7 @@ def create_expense_template(doc_name, people_names):
         adjusted_width = (max_length + 2)
         
         # Set the width of the 'Shared with' column to the width of all names
-        if openpyxl.utils.get_column_letter(i+1) == 'D':  # Assuming D is the column for "Shared with"
+        if openpyxl.utils.get_column_letter(i+1) == 'E':  # Assuming E is the column for "Shared with"
             adjusted_width = len(', '.join(people_names)) + 2
             
         sheet.column_dimensions[openpyxl.utils.get_column_letter(i+1)].width = adjusted_width
@@ -82,7 +83,8 @@ def main():
     document_name = get_document_name()
     group_size = get_group_size()
     people_names = get_person_names(group_size)
-    create_expense_template(document_name, people_names)
+    default_currency = get_default_currency()
+    create_expense_template(document_name, people_names, default_currency)
 
 if __name__ == "__main__":
     main()
